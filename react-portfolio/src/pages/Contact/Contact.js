0182f648226ac1d54c3ba114a12343126d20eb10
lauracejas <lauracejasmartin94@gmail.com>
2021-04-15 15:16:09 -0400

    get icons done

--- react-portfolio/src/pages/Contact/Contact.js
+++ react-portfolio/src/pages/Contact/Contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./contact.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub,  } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
 
@@ -12,7 +15,7 @@ const Contact = () => {
     // // Upload your resume in your drive, get the shaareable link and paste it in the resume section
     const social = {
         github: 'https://github.com/Lauracejas',
-        linkedin: 'https://linkedin.com',
+        linkedin: 'https://www.linkedin.com/in/laura-cejas-058913155/',
         resume: 'https://novoresume.com/'
     }
 
@@ -22,24 +25,21 @@ const Contact = () => {
             <h1>Get in Touch</h1>
             <div className="row">
 
-                <div className="input-group row col-12 col-sm-6 half mb-3">
+                <div className="input-group col-6 col-sm-6 half mb-3">
                     <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
                     <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
                     <textarea id="msg" name="message" placeholder="Message" required></textarea>
                     <button type="button" className="btn btn-primary">Send Message</button>
 
                 </div>
-                <div className="row col-12 col-sm-6 half mb-3">
+                <div className=" col-6 col-sm-6 half mb-3">
                     <p className="contact-me">Whether you want to get in touch, talk about a project collaboration, or just say hi, I'd love to hear from you.
-                Simply fill the from and send me an email.</p>
-                    <a title="Visit GitHub profile" href={social.github}><i className="bi bi-github"></i></a>
-                    <a title="Visit Linkedin profile" href={social.linkedin}><i className="bi bi-linkedin"></i></a>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-file-person-fill" viewBox="0 0 16 16">
-                    <a title="Visit Linkedin profile" href={social.resume}>                        
-                    <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-3 4c2.623 0 4.146.826 5 1.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-1.245C3.854 11.825 5.377 11 8 11z" /></a>
-                     
-                    </svg>
-
+                Simply fill the from and send me an email.</p>                
+               
+                    <a title="Visit GitHub profile" href={social.github}><FontAwesomeIcon icon={faGithub} /></a>
+                    <a title="Visit Linkedin profile" href={social.linkedin}><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <a title="Visit Linkedin profile" href={social.resume}><FontAwesomeIcon icon={faUser} /> </a>                     
+                
                 </div>
             </div>
         </div>
