ab4e066fe355edecca470861a3420432dfb129b3
lauracejas <lauracejasmartin94@gmail.com>
2021-05-05 16:11:58 -0400

    change about me

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -14,18 +14,18 @@ class About extends Component {
                     <img src={Photo} id="photome" alt="logo" />
                     <div className="about align-self-center col-sm-8">
                         <h1>About Me</h1>
-                        <p className="line-1 ">My name is Laura Cejas. I am from Cuba. I am a Full-Stack Software developer
-                        graduated from UNCC Boot
-                        Camp. As an
-                        organized and highly motivated person, I am able to adapt to any circumstance and always
-                        give my
-                        best in any project, at the same time I strive to work as a team and promote values.</p>
+                        <p className="line-1 ">My name is Laura Cejas. I am from Cuba. Front-end web developer
+                         leveraging psychology background to build a more intuitive user experience on the web. Recently
+                          earned a certificate in full stack development from the University of North Carolina at Charlotte , with newly developed skills
+                           in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver
+                            passionate about developing apps, with a focus on mobile-first design and development.
+                              </p>
                         <p>
-                            I enjoy being challenged and engaging with projects that requires me to step out of my
-                            comfort zone and
-                            knowledge set, as continuing to learn new languages and development techniques are
-                            important
-                            to
+                        With each project, my aim is to best engage my audience for an impactful user experience.
+                              I applied aspects of UX and agile development in a recent project. I worked on a team of
+                               four to develop a single-page MERN app that helps local business owners track their sales, 
+                               inventory, and attendance at specialty events. I’m excited
+                         to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
                     me.</p>
                     </div>
                 </div>
