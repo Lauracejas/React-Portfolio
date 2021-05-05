581333000e6d6f220c156234be9d09061301a872
lauracejas <lauracejasmartin94@gmail.com>
2021-05-05 14:40:06 -0400

    change resume link

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -118,7 +118,7 @@ const Contact = () => {
                         <ul>
                           <li>
                             <a
-                              href="https://docs.google.com/document/d/1I_7kIf3LZL7UZV9Oq56WoGlH69mZi-ZyN78S9Mu9jAs/edit?usp=sharing"
+                              href="https://www.dropbox.com/home?preview=Laura+Cejas.pdf"
                               target="_blank"
                               rel="noopener noreferrer"
                               role="button"
