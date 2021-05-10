46a2c9c85580d3f11f6ec5aff962a0fd0633347f
lauracejas <lauracejasmartin94@gmail.com>
2021-05-10 12:38:19 -0400

    update about me

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -14,17 +14,16 @@ class About extends Component {
                     <img src={Photo} id="photome" alt="logo" />
                     <div className="about align-self-center col-sm-8">
                         <h1>About Me</h1>
-                        <p className="line-1 ">My name is Laura Cejas. I am from Cuba. Front-end web developer
-                         leveraging engineering  background to build a more intuitive user experience on the web. Recently
-                          earned a certificate in full stack development from the University of North Carolina at Charlotte , with newly developed skills
-                           in JavaScript, CSS, React.js, and responsive web design. Known as an innovative problem solver
-                            passionate about developing apps, with a focus on mobile-first design and development.
-                              </p>
+                        <p className="line-1 ">I am a Front End Web Developer with a background in psychology.  I recently earned a certificate in Full Stack Development from the BootCamp of University of North Carolina at Charlotte, where I developed skills in JavaScript, HTML, CSS, React.js, and responsive web design. </p>
                         <p>
+                            <p>An innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. My degree in Engineer have prepared me to approach problems with creativity and teamwork.</p>
                         With each project, my aim is to best engage my audience for an impactful user experience.
                               I applied aspects of UX and agile development in a recent project. I’m excited
                          to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
-                    me.</p>
+                        me.</p>
+                        <p>When I’m not designing or developing, I enjoy hiking, camping, trying out a new recipe, or reading up on the latest trends in web and mobile design.
+                        </p>
+                        <p>I’m excited to leverage my skills in the future as part of a fast-paced, quality-driven team. Reach out to me to chat about our next project <a href="https://www.linkedin.com/in/laura-cejas-058913155/">here.</a></p>
                     </div>
                 </div>
             </div>
